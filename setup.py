79a5275a40ca5a282dee9a9c5e74c77f1068dec8
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2015-11-05 10:50:54 +0200

    fix(pip): Added setup.py file in order to add this SDK as a pip requirements

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='alchemyapi_python',
+    description='A sdk for AlchemyAPI using Python',
+    url='https://github.com/superdesk/alchemyapi_python'
+)
